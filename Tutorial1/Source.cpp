dca45312ff5ff6ba3a2c7b4a3dcc5fa53c352289
NRowley <nicholas.ryan.rowley@gmail.com>
2023-11-09 16:37:48 -0600

    conditional exercises

--- Tutorial1/Source.cpp
+++ Tutorial1/Source.cpp
@@ -13,6 +13,44 @@ int main()
 
 	myCharacter = 'n';
 	myInt = 12;
+	cout << myCharacter << endl;
+	cout << myInt << endl;
+	cout << "====== end exercise 1" << endl;
+
+
+	int a(13);
+	int b = 13;
+
+	if (a < b) {
+		cout << "a is less than b. " << endl;
+	}
+	if (a > b) {
+		cout << "b is less than a. " << endl;
+	}
+	cout << "====== end exercise 2" << endl;
+
+
+	if (a < b) {
+		cout << "a is less than b. " << endl;
+	}
+	else {
+		cout << "a is not less than b. " << endl;
+	}
+	cout << "====== end exercise 3" << endl;
+
+
+	if (a < b) {
+		cout << "a is less than b. " << endl;
+	}
+	else if (a > b) {
+		cout << "a is not less than b. " << endl;
+	}
+	else {
+		cout << "a is equal to b. " << endl;
+	}
+	cout << "====== end exercise 4" << endl;
+
+
 
 
 	system("pause");
